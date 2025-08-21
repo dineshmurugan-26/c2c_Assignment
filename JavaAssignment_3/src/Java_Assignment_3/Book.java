f7ea2cffdc83645a58e8379da1cfdec654889f44
ASUS <ASUS@ASUSA15>
2025-08-21 11:15:26 +0530

    assiagnment 3

---
+++ JavaAssignment_3/src/Java_Assignment_3/Book.java
@@ -0,0 +1,28 @@
+package Java_Assignment_3;
+
+public class Book extends Item {
+	private String author;
+
+	public Book(int id, String title, String author) {
+		super(id, title);
+		this.author = author;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [author=" + author + ", id=" + id + ", title=" + title + "]";
+	}
+	
+	
+	
+	
+
+}
