a6857583f53ec065db93a2c063f502edba8274b9
ASUS <ASUS@ASUSA15>
2025-08-21 11:39:18 +0530

    Banking system

---
+++ BankingSystem/src/BankingSystem_Assignment/Bank.java
@@ -0,0 +1,15 @@
+package BankingSystem_Assignment;
+
+public class Bank {
+	private static int totalAccounts=0;
+	
+	
+	public static void incrementAccount() {
+		totalAccounts++;
+	}
+	
+	public static int getTotalAccounts() {
+		return totalAccounts;
+	}
+
+}
